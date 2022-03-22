50301ca927d55beb6ea05adcbaf9f40911b7c38e
Anton <Antosah1@mail.ru>
2022-03-22 22:36:52 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/examenservice/ExamenserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.examenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
