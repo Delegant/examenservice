5e9f8fca8a2fec428b63bd9fdc83c638e41d906e
Anton <Antosah1@mail.ru>
2022-03-30 00:24:54 +0300

    add tests with mock - ExaminerServiceImplTest

--- src/test/java/pro/sky/java/course2/examenservice/constants/ExaminerServiceApplicationConstants.java
+++ src/test/java/pro/sky/java/course2/examenservice/constants/ExaminerServiceApplicationConstants.java
@@ -15,11 +15,4 @@ public class ExaminerServiceApplicationConstants {
     public final static String SECOND_CORRECT_STRING_QUESTION = "SECOND_CORRECT_STRING_QUESTION?";
     public final static String SECOND_CORRECT_STRING_ANSWER = "SECOND_CORRECT_STRING_ANSWER.";
     public final static Question SECOND_CORRECT_QUESTION = new Question(SECOND_CORRECT_STRING_QUESTION, SECOND_CORRECT_STRING_ANSWER);
-    public final static AbstractMap<String,Question> CORRECT_QUESTION_MAP = new HashMap<>(Map.of(
-            CORRECT_STRING_QUESTION,
-            CORRECT_QUESTION,
-            SECOND_CORRECT_STRING_QUESTION,
-            SECOND_CORRECT_QUESTION));
-    public final static Collection<Question> CORRECT_QUESTION_COLLECTION = Map.copyOf(CORRECT_QUESTION_MAP).values();
-
 }
--- src/test/java/pro/sky/java/course2/examenservice/service/JavaQuestionServiceTest.java
+++ src/test/java/pro/sky/java/course2/examenservice/service/JavaQuestionServiceTest.java
@@ -2,49 +2,76 @@ package pro.sky.java.course2.examenservice.service;
 
 import static pro.sky.java.course2.examenservice.constants.ExaminerServiceApplicationConstants.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pro.sky.java.course2.examenservice.domain.Question;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaQuestionServiceTest {
 
-    JavaQuestionService out = new JavaQuestionService();
+    private JavaQuestionService out;
+
+    @BeforeEach
+    private void initialize(){
+        out = new JavaQuestionService();
+    }
 
     @Test
     void shouldReturnQuestionWhenAddCorrectStringQuestionAndAnswer() {
-        assertEquals(ZERO, out.getSize());
         assertEquals(CORRECT_QUESTION, out.add(CORRECT_STRING_QUESTION, CORRECT_STRING_ANSWER));
-        assertEquals(ONE, out.getSize());
+        assertTrue(out.getAll().contains(CORRECT_QUESTION));
     }
 
     @Test
     void shouldReturnQuestionWhenAddCorrectQuestion() {
-        assertEquals(ZERO, out.getSize());
         assertEquals(CORRECT_QUESTION, out.add(CORRECT_QUESTION));
+        assertTrue(out.getAll().contains(CORRECT_QUESTION));
+    }
+
+
+    @Test
+    void shouldReturnCollectionWithoutRemovedQuestion() {
+        out.add(CORRECT_STRING_QUESTION, CORRECT_STRING_ANSWER);
+        assertEquals(CORRECT_QUESTION, out.remove(CORRECT_STRING_QUESTION));
+        assertFalse(out.getAll().contains(CORRECT_QUESTION));
+    }
+
+    @Test
+    void shouldReturnCollectionWithAddedQuestion() {
+        out.add(CORRECT_QUESTION);
+        out.add(SECOND_CORRECT_QUESTION);
+        Collection<Question> expected = List.of(CORRECT_QUESTION, SECOND_CORRECT_QUESTION);
+        assertTrue(out.getAll().containsAll(expected) && expected.containsAll(out.getAll()));
+    }
+
+    @Test
+    void shouldReturnRandomQuestion() {
+        out.add(CORRECT_QUESTION);
+        out.add(SECOND_CORRECT_QUESTION);
+        Collection<Question> expected = List.of(CORRECT_QUESTION, SECOND_CORRECT_QUESTION);
+        assertTrue(expected.contains(out.getRandomQuestion()));
+    }
+
+    @Test
+    void shouldReturnSizeWhenAddDifferentQuestions() {
+        assertEquals(ZERO, out.getSize());
+        assertEquals(CORRECT_QUESTION, out.add(CORRECT_STRING_QUESTION, CORRECT_STRING_ANSWER));
+        assertEquals(ONE, out.getSize());
+        assertEquals(SECOND_CORRECT_QUESTION, out.add(SECOND_CORRECT_STRING_QUESTION, SECOND_CORRECT_STRING_ANSWER));
+        assertEquals(TWO, out.getSize());
+    }
+
+    @Test
+    void shouldReturnSizeOneWhenAddTwoEqualQuestion() {
+        assertEquals(ZERO, out.getSize());
+        assertEquals(CORRECT_QUESTION, out.add(CORRECT_STRING_QUESTION, CORRECT_STRING_ANSWER));
+        assertEquals(CORRECT_QUESTION, out.add(CORRECT_STRING_QUESTION, CORRECT_STRING_ANSWER));
         assertEquals(ONE, out.getSize());
     }
 
-
-    @Test
-    void remove() {
-        out.add(CORRECT_STRING_QUESTION, CORRECT_STRING_ANSWER);
-        assertEquals(CORRECT_QUESTION, out.remove(CORRECT_STRING_QUESTION));
-        assertEquals(ZERO, out.getSize());
-    }
-
-    @Test
-    void getAll() {
-        out.add(CORRECT_STRING_QUESTION, CORRECT_STRING_ANSWER);
-        out.add(SECOND_CORRECT_STRING_QUESTION, SECOND_CORRECT_STRING_ANSWER);
-        assertEquals(TWO, out.getSize());
-        assertEquals(CORRECT_QUESTION_COLLECTION, out.getAll());
-    }
-
-    @Test
-    void getRandomQuestion() {
-    }
-
-    @Test
-    void getSize() {
-    }
 }
